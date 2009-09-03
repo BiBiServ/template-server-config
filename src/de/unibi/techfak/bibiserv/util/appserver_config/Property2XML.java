d79fe571070c06f19bf8e63f9e0bc95e9e465a30
jkrueger <jkrueger@dyn-161-79.CeBiTec.Uni-Bielefeld.DE>
2009-09-03 17:01:38 +0200

    fix some really 'stupid' bugs within the tasks - never write a piece of code if you're overtired ...

--- src/de/unibi/techfak/bibiserv/util/appserver_config/Property2XML.java
+++ src/de/unibi/techfak/bibiserv/util/appserver_config/Property2XML.java
@@ -2,11 +2,11 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package de.unibi.techfak.bibiserv.util.appserver_config;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -17,9 +17,9 @@ import org.apache.tools.ant.Task;
  *
  * @author jkrueger
  */
-public class Property2XML extends Task{
+public class Property2XML extends Task {
 
-        private String src = null;
+    private String src = null;
     private File src_file = null;
     private String dest = null;
     private File dest_file = null;
@@ -34,7 +34,7 @@ public class Property2XML extends Task{
 
     @Override
     public void execute() throws BuildException {
-        if (src == null) {
+        if (src != null) {
             src_file = new File(src);
             if (!src_file.isFile()) {
                 throw new BuildException("Srcfile +'" + src_file + " not found .");
@@ -45,13 +45,21 @@ public class Property2XML extends Task{
         //read properties from file
         try {
             prop.load(new FileInputStream(src_file));
-            // write properties to file
-            prop.storeToXML(new FileOutputStream(new File(dest)),"converted by "+getClass().getName());
-
-         } catch (IOException e) {
+        } catch (FileNotFoundException e) {
+            throw new BuildException("Src '" + src + "' not found ... ", e);
+        } catch (IOException e) {
             throw new BuildException(e);
         }
 
-    }
+        // write properties to file
+        try {
+            prop.storeToXML(new FileOutputStream(new File(dest)), "converted by " + getClass().getName());
+        } catch (FileNotFoundException e) {
+            throw new BuildException("Dest '" + src + "' not found ... ", e);
+        } catch (IOException e) {
+            throw new BuildException(e);
+        }
 
+        System.out.println("Read properties from '"+src+"' and write XML properties to '"+dest+"'");
+    }
 }
--- src/de/unibi/techfak/bibiserv/util/appserver_config/XML2Property.java
+++ src/de/unibi/techfak/bibiserv/util/appserver_config/XML2Property.java
@@ -6,8 +6,10 @@ package de.unibi.techfak.bibiserv.util.appserver_config;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
@@ -23,17 +25,17 @@ public class XML2Property extends Task {
     private String dest = null;
     private File dest_file = null;
 
-    private void setSrc(String src) {
+    public void setSrc(String src) {
         this.src = src;
     }
 
-    private void setDest(String dest) {
+    public void setDest(String dest) {
         this.dest = dest;
     }
 
     @Override
     public void execute() throws BuildException {
-        if (src == null) {
+        if (src != null) {
             src_file = new File(src);
             if (!src_file.isFile()) {
                 throw new BuildException("Srcfile +'" + src_file + " not found .");
@@ -43,13 +45,24 @@ public class XML2Property extends Task {
         Properties prop = new Properties();
         //read properties from file
         try {
-            prop.loadFromXML(new FileInputStream(src_file));
-            // write properties to file
-            prop.store(new FileOutputStream(new File(dest)),"converted by "+getClass().getName());
-
-         } catch (IOException e) {
+            InputStream in = new FileInputStream(src_file);
+            prop.loadFromXML(in);
+           
+        } catch (FileNotFoundException e) {
+            throw new BuildException("Src '"+src+"' not found ... ",e);
+        } catch (IOException e) {
             throw new BuildException(e);
         }
 
+         // write properties to file
+        try {
+            prop.store(new FileOutputStream(new File(dest)), "converted by " + getClass().getName());
+        } catch (FileNotFoundException e){
+            throw new BuildException("Dest '"+dest+"' not found ... ",e);
+        } catch (IOException e){
+            throw new BuildException(e);
+        }
+
+        System.out.println("Read XML properties from '"+src+"' and write properties to '"+dest+"'");
     }
 }
