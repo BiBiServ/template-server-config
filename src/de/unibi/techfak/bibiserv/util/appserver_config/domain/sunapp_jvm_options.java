30c0ebd5558f90afc8993a10b6dafe71cdeb6aa0
jkrueger <jkrueger@dyn-161-79.CeBiTec.Uni-Bielefeld.DE>
2009-12-10 14:47:26 +0100

    add java_config/jvm_options representation

---
+++ src/de/unibi/techfak/bibiserv/util/appserver_config/domain/sunapp_jvm_options.java
@@ -0,0 +1,26 @@
+package de.unibi.techfak.bibiserv.util.appserver_config.domain;
+
+/**
+ * Simpel representation of an JVM Option Element
+ *
+ * @author jkrueger
+ */
+public class sunapp_jvm_options {
+
+    private String value;
+
+    public sunapp_jvm_options(){
+        
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+
+
+}
