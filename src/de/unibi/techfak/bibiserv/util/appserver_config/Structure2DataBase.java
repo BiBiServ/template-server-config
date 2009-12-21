4725da6fa742a8bd1477479d0a2c5b2490947f20
jkrueger <jkrueger@zingana.CeBiTec.Uni-Bielefeld.DE>
2009-12-21 10:03:48 +0100

    Task that add an bibiserv structure file to the database (previous XML2Database)

---
+++ src/de/unibi/techfak/bibiserv/util/appserver_config/Structure2DataBase.java
@@ -0,0 +1,101 @@
+package de.unibi.techfak.bibiserv.util.appserver_config;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+/**
+ *
+ * @author Daniel Hagemeier - dhagemei(at)cebitec.uni-bielefeld.de
+ *         Jan Krüger - jkrueger(at)cebitec.uni-bielefeld.de
+ */
+public class Structure2DataBase extends Task {
+
+    private String src = null;
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+    private String dbURL = null;
+
+    public void setDbURL(String dbURL) {
+        this.dbURL = dbURL;
+    }
+    private Connection con = null;
+    private PreparedStatement stmt = null;
+
+    @Override
+    public void execute() throws BuildException {
+        if (dbURL == null) {
+            throw new BuildException("Attribute 'dbURL' is mandantory und must be valid jdbcURL!");
+        }
+        if (src == null) {
+            throw new BuildException("Attribute 'src' is mandantory!");
+        }
+        File src_file = new File(src);
+        if (!src_file.isFile()) {
+            throw new BuildException("'src'file +'" + src_file + " not found .");
+        }
+        // create connection
+
+
+
+        FileReader ir = null;
+
+
+
+
+        try {
+            createConnection();
+            stmt = con.prepareStatement("INSERT INTO STRUCTURE (TIME, CONTENT) VALUES (CURRENT_TIMESTAMP, (? AS CLOB))");
+            stmt.setClob(1, new BufferedReader(new FileReader(src_file)));
+            stmt.execute();
+            System.out.println("Written Structure of BiBiServ to '"+dbURL+"' into table STRUCTURE");
+        } catch (SQLException ex) {
+            Logger.getLogger(Structure2DataBase.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Structure2DataBase.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                ir.close();
+                shutdown();
+            } catch (IOException ex) {
+                Logger.getLogger(Structure2DataBase.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private void createConnection() {
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+            //Get a connection
+            con = DriverManager.getConnection(dbURL);
+        } catch (Exception except) {
+            except.printStackTrace();
+        }
+    }
+
+    private void shutdown() {
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (con != null) {
+                DriverManager.getConnection(dbURL + ";shutdown=true");
+                con.close();
+            }
+        } catch (SQLException sqlExcept) {
+        }
+
+    }
+}
